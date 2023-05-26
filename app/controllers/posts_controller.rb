e0743f6e6ef01faebb2dfc83832ec1065d0ece00
Federico Sapuppo <fedesapuppo@hotmail.com>
2023-05-26 16:36:36 +0300

    modified posts_controller.rb and new.html.erb to link Post with User and have autor as user's email by default

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,26 +1,28 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
 
-        def new
-          @post = Post.new
-        end
-      
-        def create
-          @post = Post.new(post_params)
-          if @post.save
-            redirect_to posts_path, notice: 'Post created successfully!'
-          else
-            render :new
-          end
-        end
-      
-        def index
-          @posts = Post.all
-        end
-      
-        private
-      
-        def post_params
-          params.require(:post).permit(:title, :content, :author)
-        end
-      
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+
+    if @post.save
+      redirect_to posts_path, notice: "Post created successfully."
+    else
+      render :new
+    end
+  end
+
+
+  def index
+    @posts = Post.all
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content, :author)
+  end
 end
